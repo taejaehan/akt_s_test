0cfc10238932dc2d60df2263dc396febb5202a73
TaejaeHan <hantaejae@gmail.com>
2018-02-26 20:20:56 +0900

    test

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,6 @@
 #include "ofApp.h"
 #include "ofAppNoWindowRun.h"
 #include "ofAppNoWindow.h"
-#include "ofAppGlutWindow.h"
-#include "ofAppGLFWWindow.h"
-
 //========================================================================
 int main( ){
 //    ofSetupOpenGL(1024,768,OF_WINDOW);            // <-------- setup the GL context
